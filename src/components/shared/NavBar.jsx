d1bb2030c16a2202e41c545e2643a1e076920b35
prajwal zingare <prajwalzingre@gmail.com>
2023-05-12 21:08:11 +0530

    style:navbar added

--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -1,28 +1,46 @@
 import React from "react";
+// import PermIdentityOutlinedIcon from "@mui/icons-material/PermIdentityOutlined";
 import "./navbar.css";
+import {
+  PermIdentityOutlinedIcon,
+  FavoriteBorderIcon,
+  ShoppingCartOutlinedIcon,
+  SearchOutlinedIcon,
+} from "assets";
+
 function NavBar() {
   return (
     <>
       <nav className="nav-container">
         <div className="nav-left">
-          <p>Organico</p>
-          <p>Home</p>
-          <p>Buy Now</p>
+          <div className="brand">
+            <p className="brand-name">Organico</p>
+          </div>
+          <a className="buy-now" href="/">
+            Buy Now
+          </a>
         </div>
 
         <div className="nav-right">
           <div className="searchbar-container">
-            <input type="text" />
+            <input
+              type="text"
+              placeholder="Search for Item"
+              className="nav-search-field"
+            />
+            <button className="btn-no-decoration">
+              <SearchOutlinedIcon className="nav-icon" />
+            </button>
           </div>
           <div className="nav-icons-container">
-            <div className="nav-icon">
-              <i class="fa-solid fa-heart"></i>
+            <div className="nav-icons">
+              <PermIdentityOutlinedIcon className="nav-icon" />
             </div>
-            <div className="nav-icon">
-              <i class="fa-solid fa-cart-shopping"></i>
+            <div className="nav-icons">
+              <FavoriteBorderIcon className="nav-icon" />
             </div>
-            <div className="nav-icon">
-              <i class="fa-solid fa-heart"></i>
+            <div className="nav-icons ">
+              <ShoppingCartOutlinedIcon className="nav-icon" />
             </div>
           </div>
         </div>
