c45017eaf04d8ad857d746e99a46eefbd6ebc60c
prajwal zingare <prajwalzingre@gmail.com>
2023-05-13 15:57:27 +0530

    feat: Improved navbar functionality and design

--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -11,38 +11,50 @@ import {
 function NavBar() {
   return (
     <>
-      <nav className="nav-container">
-        <div className="nav-left">
-          <div className="brand">
-            <p className="brand-name">Organico</p>
+      <nav>
+        <div className="nav-container">
+          <div className="nav-left">
+            <div className="brand">
+              <p className="brand-name">Organico</p>
+            </div>
+            <a className="buy-now" href="/">
+              Buy Now
+            </a>
           </div>
-          <a className="buy-now" href="/">
-            Buy Now
-          </a>
-        </div>
 
-        <div className="nav-right">
-          <div className="searchbar-container">
-            <input
-              type="text"
-              placeholder="Search for Item"
-              className="nav-search-field"
-            />
-            <button className="btn-no-decoration">
-              <SearchOutlinedIcon className="nav-icon" />
-            </button>
+          <div className="nav-right">
+            <div className="searchbar-container">
+              <input
+                type="text"
+                placeholder="Search for Item"
+                className="nav-search-field"
+              />
+              <button className="btn-no-decoration">
+                <SearchOutlinedIcon className="nav-icon" />
+              </button>
+            </div>
+            <div className="nav-icons-container">
+              <div className="nav-icons">
+                <PermIdentityOutlinedIcon className="nav-icon" />
+              </div>
+              <div className="nav-icons">
+                <FavoriteBorderIcon className="nav-icon" />
+              </div>
+              <div className="nav-icons ">
+                <ShoppingCartOutlinedIcon className="nav-icon" />
+              </div>
+            </div>
           </div>
-          <div className="nav-icons-container">
-            <div className="nav-icons">
-              <PermIdentityOutlinedIcon className="nav-icon" />
-            </div>
-            <div className="nav-icons">
-              <FavoriteBorderIcon className="nav-icon" />
-            </div>
-            <div className="nav-icons ">
-              <ShoppingCartOutlinedIcon className="nav-icon" />
-            </div>
-          </div>
+        </div>
+        <div className="mobile-search-container">
+          <input
+            type="text"
+            placeholder="Search for items"
+            className="nav-search-field"
+          />
+          <button type="submit" className="btn-no-decoration">
+            <SearchOutlinedIcon className="nav-icon" />
+          </button>
         </div>
       </nav>
     </>
